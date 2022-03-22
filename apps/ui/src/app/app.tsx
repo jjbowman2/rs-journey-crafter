c1fc5d39d2926daf73b84ad848dc32144a0652aa
Jedde Bowman <jjbowma2@gmail.com>
2022-03-21 19:56:45 -0700

    Added UI

---
+++ apps/ui/src/app/app.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+        <div className="flex min-h-screen flex-col justify-center bg-slate-700">
+            <div className="mx-auto h-96 w-96 rounded-md bg-gray-200">
+                <h1 className="animate-pulse p-8 text-2xl uppercase tracking-widest text-slate-600">Welcome</h1>
+                <p className="px-8 text-3xl text-slate-800">This is the start of something great</p>
+            </div>
+        </div>
+    );
+}
